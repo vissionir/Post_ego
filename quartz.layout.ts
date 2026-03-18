e6936845594aa5e9837318ac42f6cc5f83b2c347
Денис Алещев <denisalesev@MacBook-Air-Denis-5.local>
2026-03-18 21:50:57 +0700

    Replace Quartz footer branding with Post-Ego footer

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -6,12 +6,7 @@ export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
   afterBody: [],
-  footer: Component.Footer({
-    links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
-    },
-  }),
+  footer: Component.Footer(),
 }
 
 const baseExplorerSort = (a: any, b: any) => {
