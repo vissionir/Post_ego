34490da66febd276ac78ba2540812fccb5de967a
Денис Алещев <denisalesev@MacBook-Air-Denis-5.local>
2026-03-24 15:55:48 +0700

    Refine site icon for browser and Apple surfaces

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -90,6 +90,8 @@ export default (() => {
         )}
 
         <link rel="icon" href={iconPath} />
+        <link rel="shortcut icon" href={iconPath} />
+        <link rel="apple-touch-icon" href={iconPath} />
         <meta name="description" content={description} />
         <meta name="generator" content="Quartz" />
 
